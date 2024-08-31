36e52f9ea8ed752c326bd763db29b6085ef52094
Kyrmasch <Kyrmasch@gmail.com>
2024-08-31 19:50:56 +0500

    FP-3 - requirements.txt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from dask.distributed import Client
 import dask.array as da
 import cupy as cp
+import gc
 
 app = FastAPI()
 client = Client("tcp://localhost:8786")
@@ -10,9 +11,12 @@ client = Client("tcp://localhost:8786")
 @app.get("/gpu-calculate")
 async def gpu_calculate():
     try:
-        x = da.from_array(cp.random.rand(10000, 10000), chunks=(1000, 1000))
+        x = da.from_array(cp.random.rand(100, 100), chunks=(100, 100))
         result = (x @ x.T).sum(axis=0).compute()
         result_cpu = cp.asnumpy(result)
+
+        cp.get_default_memory_pool().free_all_blocks()
+
         return {"result": result_cpu.tolist()}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
