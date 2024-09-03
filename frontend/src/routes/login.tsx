53421690bd629d4ba536ed737536607a5f70e305
Kyrmasch <Kyrmasch@gmail.com>
2024-09-03 20:04:05 +0500

    FP-16 - Login page

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,3 +1,12 @@
+import "./login.scss";
+
 export default function LoginPage() {
-  return <></>;
+  return (
+    <div className="login">
+      <div className="left">
+        <h1 className="title">FLOOD ANALITIC</h1>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bg-image': "url('/bg-image.png')",
+      },
+      fontFamily: {
+        'sans': ['Helvetica', 'Arial', 'sans-serif']
+      }
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
