74835dbb3b5e17540f28a689276ca7e0afb48441
Kyrmasch <Kyrmasch@gmail.com>
2024-09-30 10:47:52 +0500

    FP-43 - Связанные данные для таблицы

--- backend/routers/data.py
+++ backend/routers/data.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from usecases.metadata.get_meta import get_model_by_tablename
 from geoalchemy2.elements import WKBElement
 from sqlalchemy import func
+from sqlalchemy.orm import joinedload
 
 data_router = APIRouter()
 
@@ -21,11 +22,24 @@ async def get_table_data(
     db: Session = Depends(get_db),
 ):
 
-    def row_to_dict(row):
+    def row_to_dict(row, visited=None):
+        if visited is None:
+            visited = set()
+
+        if id(row) in visited:
+            return None
+
+        visited.add(id(row))
+
         data = {}
         for c in row.__table__.columns:
             value = getattr(row, c.name)
             data[c.name] = wkb_to_geojson(value)
+
+        for relationship in row.__mapper__.relationships:
+            related_data = getattr(row, relationship.key)
+            if related_data is not None and not isinstance(related_data, list):
+                data[relationship.key] = row_to_dict(related_data, visited)
         return data
 
     def wkb_to_geojson(wkb):
@@ -38,12 +52,12 @@ async def get_table_data(
     if not model:
         raise HTTPException(status_code=404, detail="Модель не найдена")
 
+    query = select(model).options(joinedload("*")).limit(limit).offset(offset)
+    result = db.execute(query)
+    rows = result.unique().scalars().all()
+
     count_query = select(func.count()).select_from(model)
     total_count = db.execute(count_query).scalar()
-
-    query = select(model).limit(limit).offset(offset)
-    result = db.execute(query)
-    rows = result.scalars().all()
 
     data = [row_to_dict(row) for row in rows]
 
--- frontend/src/domain/interfaces/meta.ts
+++ frontend/src/domain/interfaces/meta.ts
@@ -1,6 +1,7 @@
 export interface IColumnMeta {
   name: string;
   type: string;
+  is_rel?: boolean;
 }
 
 export interface IRelationshipMeta {
