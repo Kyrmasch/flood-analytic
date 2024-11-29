4734573fcd9d05feeb7d1abccc39b87bc39025d2
Shynggys <a.shynggys@pin-up.tech>
2024-11-29 15:53:15 +0700

    FP-37 save geojson data

--- backend/routers/calc.py
+++ backend/routers/calc.py
@@ -4,7 +4,7 @@ from deps import get_current_user_with_role_factory
 from schemas.auth import User as UserSchema
 from infrastructure.websocket import websocket_manager, WebSocketManager
 import dask.array as da
-import cupy as cp
+# import cupy as cp
 from infrastructure.dask import DaskManager, dask_manager
 
 calc_router = APIRouter()
@@ -16,23 +16,23 @@ async def get_devises(dc: DaskManager = Depends(lambda: dask_manager)):
     return {"devices": list}
 
 
-@calc_router.get("/calc")
-async def gpu_calculate(
-    dc: DaskManager = Depends(lambda: dask_manager),
-    current_user: UserSchema = Depends(get_current_user_with_role_factory(["admin"])),
-):
-    try:
-        if dc.client:
-            x = da.from_array(cp.random.rand(100, 100), chunks=(100, 100))
-            result = (x @ x.T).sum(axis=0).compute()
-            result_cpu = cp.asnumpy(result)
+# @calc_router.get("/calc")
+# async def gpu_calculate(
+#     dc: DaskManager = Depends(lambda: dask_manager),
+#     current_user: UserSchema = Depends(get_current_user_with_role_factory(["admin"])),
+# ):
+#     try:
+#         if dc.client:
+#             x = da.from_array(cp.random.rand(100, 100), chunks=(100, 100))
+#             result = (x @ x.T).sum(axis=0).compute()
+#             result_cpu = cp.asnumpy(result)
 
-            cp.get_default_memory_pool().free_all_blocks()
+#             cp.get_default_memory_pool().free_all_blocks()
 
-            return {"result": result_cpu.tolist()}
-    except Exception as e:
-        pass
-    raise HTTPException(status_code=500, detail=str(e))
+#             return {"result": result_cpu.tolist()}
+#     except Exception as e:
+#         pass
+#     raise HTTPException(status_code=500, detail=str(e))
 
 
 @calc_router.get("/send/{client_id}")
---
+++ backend/schemas/map.py
@@ -0,0 +1,10 @@
+
+from pydantic import BaseModel
+from typing import List
+
+class GeoJSONData(BaseModel):
+    type: str
+    features: List[dict]
+
+    class Config:
+        orm_mode = True
